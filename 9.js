7b69fcca8897c6850c0fb40a5bc5d2b8731c29ab
Raghav <raghav2004kaushal@gmail.com>
2025-07-17 10:39:58 +0530

    17th July 2025

---
+++ 9.js
@@ -0,0 +1,84 @@
+// const https = require("node:https");
+// const fs = require("node:fs");
+
+// fs.readFile("./something.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// setTimeout(() => {
+//   console.log("THIS IS SETTIMEOUT");
+// }, 0);
+
+// https
+//   .get("https://encrypted.google.com/", (res) => {
+//     console.log("THIS IS API DATA");
+//   })
+//   .on("error", (e) => {
+//     console.error(e);
+//   });
+
+// setImmediate(() => {
+//   console.log("SET IMMEDIATE");
+// });
+
+// setTimeout(() => {
+//   console.log("THIS IS SETTIMEOUT");
+// }, 0);
+
+// process.nextTick(() => {
+//   console.log("NEXT TICK");
+// });
+
+// console.log("HELLOOOO");
+
+// process.nextTick(() => {
+//   console.log("NEXT TICK 2");
+// });
+
+////////////////////////////////////////////////////////////////////////
+
+// Event Emitter
+
+// const EventEmitter = require("node:events");
+// const eventEmitter = new EventEmitter();
+
+// eventEmitter.on("event1", () => {
+//   console.log("bbbbbbb");
+// });
+
+// eventEmitter.emit("event1");
+
+// eventEmitter.emit("event1");
+
+// eventEmitter.once("event2", () => {
+//   console.log("vvvvvvv");
+// });
+
+// eventEmitter.emit("event2");
+
+// eventEmitter.emit("event2");
+
+// eventEmitter.off("event3", () => {gh
+//   console.log("9999999");
+// }); 
+
+// eventEmitter.emit("event3");
+
+
+
+//////////////////////////////////////////////////////////
+
+// Node js moduels inbuild
+
+//////////////////////////////////////////////////////////
+
+// DNS    ----> lookup
+//        ----> reverse lookup
+
+
+
+
